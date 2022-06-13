bc8c7eb7492100cd01286a16c9f563b058e54d51
Somogyi Krisztian <krisztian@somogyi.xyz>
2022-06-13 05:01:12 +0200

    Customize

--- locustfile.py
+++ locustfile.py
@@ -7,13 +7,3 @@ class QuickstartUser(HttpUser):
     @task
     def hello_world(self):
         self.client.get("/")
-#        self.client.get("/world")
-
-#    @task(3)
-#    def view_items(self):
-#        for item_id in range(10):
-#            self.client.get(f"/item?id={item_id}", name="/item")
-#            time.sleep(1)
-
-#    def on_start(self):
-#        self.client.post("/login", json={"username":"foo", "password":"bar"})
